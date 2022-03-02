a2e14ea1c1a0629146e8e4ea405f72944d839fea
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-03-02 10:59:28 +0100

    Add SimpleCircularList

--- pps-lab01-tdd/test/CircularListTest.java
+++ pps-lab01-tdd/test/CircularListTest.java
@@ -1,4 +1,7 @@
+import lab01.tdd.CircularList;
+import lab01.tdd.SimpleCircularList;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
@@ -7,11 +10,13 @@ import org.junit.jupiter.api.Test;
  */
 public class CircularListTest {
 
-    //TODO: test implementation
+    private CircularList circularList;
 
-    @Disabled
-    @Test public void testTodo(){
-        Assertions.fail();
+    @BeforeEach
+    void beforeEach(){
+        circularList = new SimpleCircularList();
     }
+
+
 
 }
